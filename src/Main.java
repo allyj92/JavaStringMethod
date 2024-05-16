070c58ca52d5620bb2436189323bcb3535609b42
eunjaelim <imeunjae361@gmail.com>
2024-05-16 12:36:43 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    String name = "     Eunjae      ";
+
+//    boolean result = name.equalsIgnoreCase("eunjae");
+//        int result = name.length();
+//        char result =name.charAt(0);
+//        int result = name.indexOf('n');
+//        boolean result = name.isEmpty();
+//        String result = name.toUpperCase();
+//        String result = name.toLowerCase();
+//        String result = name.trim();
+        String result = name.replace('E','e');
+        System.out.println(result);
+    }
+}
